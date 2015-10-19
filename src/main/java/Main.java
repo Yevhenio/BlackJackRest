cf531745dcab1b3226f54d2af05c742ee6f7f4f8
Yevhenio <evkomail00@gmail.com>
2015-10-19 14:21:09 +0300

    cleaned Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,36 +12,6 @@ import java.util.Random;
  */
 public class Main {
     public static void main(String[] args) {
-        UserRestService us = new UserRestService();
-        UserDAO ud = new UserDAO();
-        //User le = new User(1111);
-
-
-        //System.out.println(le.toString());
-
-
-        //u.addUser(le);
-        CardService cs = new CardService();
-
-        Random r = new Random();
-        System.out.println(cs.getCards().get(r.nextInt(52)));
-        /*for (int i = 0; i < cs.getCards().size(); i++) {
-            Card c = (Card) cs.getCards().get(i);
-            System.out.println(c.toString()) ;
-        }*/
-        CardsRestService csr = new CardsRestService();
-        csr.login(1111);
-        csr.start(10);
-        System.out.println(csr.show());
-        System.out.println("Dealer:" + "\n");
-        System.out.println(csr.showDealer());
-        System.out.println(us.currentUser);
-        System.out.println(us.player);
-        csr.getResult();
-
-        ArrayList print = new ArrayList();
-        print.add(ud.getTransactions(1111));
-        System.out.println(print);
 
     }
 
